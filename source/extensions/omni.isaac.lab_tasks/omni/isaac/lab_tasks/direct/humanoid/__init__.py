33e8fedc2e2d67ee1d47765e274f493b66040381
li-rh <li-runhua@foxmail.com>
2025-03-08 21:56:04 +0800

    学习了如何修改已有的direct类型环境，但是不知道如何配置里面的参数，比如joint_gears

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/humanoid/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/humanoid/__init__.py
@@ -26,3 +26,21 @@ gym.register(
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
 )
+
+from .h1_env import H1Env, H1EnvCfg
+
+gym.register(
+    id="Isaac-H1-Direct-v0",
+    entry_point="omni.isaac.lab_tasks.direct.humanoid:H1Env",
+    disable_env_checker=True,
+    # '''
+    # 如果任务中的变化很小，则很可能可以使用相同的 RL 库智能体配置成功训练它。
+    # 否则，建议创建新的配置文件（在 kwargs 参数下注册时调整其名称），以避免更改原始配置。
+    # '''
+    kwargs={
+        "env_cfg_entry_point": H1EnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
